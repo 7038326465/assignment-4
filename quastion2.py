d55dc0ee5286dca3682637ae9e04d704e92b5d39
Akash borade <103836603+7038326465@users.noreply.github.com>
2022-06-05 22:25:21 +0530

    Add files via upload

---
+++ quastion2.py
@@ -0,0 +1,5 @@
+#Python program to triple all numbers of a given list of integers. Use Python map
+num_list =[1, 2, 3, 4, 5, 6, 7]
+data = list(map(lambda i:i*3, num_list))
+print(data)
+
---
+++ quastion3.py
@@ -0,0 +1,4 @@
+#Python program to square the elements of a list using map() function
+num_list=[4, 5, 2, 9]
+data = list(map(lambda i:i**2, num_list))
+print(data)
